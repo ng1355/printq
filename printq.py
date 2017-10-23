7713abfaf93b95834760d0cfe09627fa744705c7
Nikita Georgiou <nikitageorgiou@disgust.poly.edu>
2017-10-23 15:33:48 -0400

    init commit

---
+++ printq.py
@@ -0,0 +1,17 @@
+''' 
+Basic code to get flask up and running, performs templating for pages
+'''
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__) 
+
+@app.route('/', methods=['GET', 'POST'])
+def root():
+    floor = None
+    if request.method == 'POST':
+        floor = request.form['floor'] 
+    return render_template('root.html', result = floor)
+
+app.secret_key = 'bina'
+app.run('localhost', 13000, debug=True)
